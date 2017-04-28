6a2a005a838b89bd261269554325207a12c94684
Jesse Shipley <jesse.radack@gmail.com>
2017-04-28 11:22:54 -0600

    add change handler for input

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
 
 export default class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { city: '' };
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ city: event.target.value })
+  }
+   
   render() {
     return (
       <form className='input-group'>
-        <input />
+        <input
+          placeholder='Enter City' 
+          className='form-control'
+          value={this.state.city}
+          onChange={this.handleChange} 
+          />
         <span className='input-group-btn'>
           <button type='submit' className='btn btn-secondary'>Submit</button>
         </span>
