6866dcfc13a5e3894e1eed4a4d0eca3e2e42fe66
Jesse Shipley <jesse.radack@gmail.com>
2017-04-28 16:23:22 -0600

    adds parsing the JSON response into a table

--- src/containers/City_List.js
+++ src/containers/City_List.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-
-export default class CityList extends Component {
+class CityList extends Component {
+  parseCity(cityData) {
+    let iconCode = cityData.weather[0].icon
+    let iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`
+    let fahrenheit = ((cityData.main.temp - 273.15) * 1.8) + 32
+    return (
+      <tr key={cityData.id}>
+        <td><h3>{cityData.name}</h3></td>
+        <td><h3>{Math.floor(fahrenheit)}</h3></td>
+        <td><img src={iconUrl} alt='img missing'></img><br/>{cityData.weather[0].description}</td>
+      </tr>
+    )
+  }
   render() {
     return (
       <table className='table table-hover'>
         <thead>
-          <th> City </th>
+          <tr>
+            <th>City</th>
+            <th>Current Temp</th>
+            <th>Outside</th>          
+          </tr>
         </thead>
         <tbody>
+          {this.props.weather.map(this.parseCity)}
         </tbody>
       </table>
     )
   }
 }
+
+const mapStateToProps = ({ weather }) => {
+  return { weather };
+}
+
+export default connect(mapStateToProps)(CityList);
